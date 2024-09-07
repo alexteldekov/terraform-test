cd0847141b65939ef2b58f08aee6e46620d5d7dc
Alex Teldekov <alext@t7g.org>
2024-09-07 13:14:58 +0300

    tflint fixes 2

--- modules/test_module/provider.tf
+++ modules/test_module/provider.tf
@@ -1,7 +1,9 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = "=> 0.128"
     }
   }
+  required_version = ">= 1.9.5"
 }
