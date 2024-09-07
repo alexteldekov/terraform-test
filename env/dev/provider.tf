25bbf770b313732cc21520fb4208c77cebe4f6ee
Alex Teldekov <alext@t7g.org>
2024-09-07 13:14:09 +0300

    tflint fixes

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -1,9 +1,11 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = "=> 0.128"
     }
   }
+  required_version = ">= 1.9.5"
 }
 
 provider "yandex" {
